38cde41a110a076baafe1fbd442d8a111780db5d
Matthew Sanders <msanders5984@gmail.com>
2021-02-13 04:19:54 -0500

    check isShiftClick instead of item presence

--- src/main/java/com/github/ms5984/api/menuman/Menu.java
+++ src/main/java/com/github/ms5984/api/menuman/Menu.java
@@ -156,8 +156,10 @@ public final class Menu {
             if (e.getClickedInventory() == e.getView().getBottomInventory()) {
                 // and we want to cancel clicks for the bottom, cancel the event
                 if (cancelClickLower) e.setCancelled(true);
-                // if the item clicked is not null and we are not allowing shift clicks
-                if (e.getCurrentItem() != null && !allowShiftClickLower) e.setCancelled(true);
+                // if we are not allowing shift clicks
+                if (e.isShiftClick() && !allowShiftClickLower) {
+                    e.setCancelled(true);
+                }
                 return;
             }
             // If the player used hotkeys, cancel the event
